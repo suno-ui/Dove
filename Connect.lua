789fa011a19f6e25a37a0fb1469755d20a88cc9a
Suno <130535266+suno-ui@users.noreply.github.com>
2023-05-22 16:45:22 -0700

    Update Connect.lua

--- Connect.lua
+++ Connect.lua
@@ -1,3 +1,8 @@
+if (getgenv().Dove) then
+    return warn("Dove is already running, please close your Roblox instance, and try again.")
+end
+getgenv().Dove = true
+
 local services = {
     ["HttpService"] = game:GetService("HttpService"),
     ["Players"] = game:GetService("Players")
