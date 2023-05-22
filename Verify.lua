ebd80c9f07b7dde947b15c110c7799c5e45bb262
Suno <130535266+suno-ui@users.noreply.github.com>
2023-05-22 16:17:28 -0700

    Create Verify.lua

---
+++ Verify.lua
@@ -0,0 +1,24 @@
+local whitelistedUsers = loadstring(game:HttpGet("https://raw.githubusercontent.com/suno-ui/Dove/main/Roles.lua", true))()
+
+local services = {
+    ["Players"] = game:GetService("Players"),
+}
+
+local verified = false
+
+local localPlayer = services.Players.LocalPlayer;
+local userId = localPlayer.UserId
+
+for i = 1, #whitelistedUsers do
+    if userId == whitelistedUsers[i] then
+        return verified == true
+    else
+        local message = Instance.new("Message", workspace)
+        message.Text = "You are not whitelisted with Dove Admin, if you think this is a mistake, please contact the owner of Dove, Suno#0526"
+    
+        task.wait(2)
+        message:Destroy()
+    
+        return localPlayer:Kick("You are not whitelisted thru Dove.")
+    end
+end
