cf47eae36fcab83441fb252441b946bcd78bc3b4
Suno <130535266+suno-ui@users.noreply.github.com>
2023-05-22 16:34:42 -0700

    Update Verify.lua

--- Verify.lua
+++ Verify.lua
@@ -13,7 +13,7 @@ local userId = localPlayer.UserId
 
 for i = 1, #whitelistedUsers do
     if userId == whitelistedUsers[i] then
-        return print("User was marked off as whitelisted.") and loadstring(game:HttpGet("", true))()
+        return print("User was marked off as whitelisted.") and loadstring(game:HttpGet("https://raw.githubusercontent.com/suno-ui/Dove/main/Connect.lua", true))()
     else
         local message = Instance.new("Message", workspace)
         message.Text = "You are not whitelisted with Dove Admin, if you think this is a mistake, please contact the owner of Dove, Suno#0526"
